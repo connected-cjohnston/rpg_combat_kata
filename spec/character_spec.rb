61864b34782728e28275723c6f70678b91ec88fa
Chris Johnston <cjohnston@connected.io>
2022-12-07 09:13:57 -0500

    iteration 1

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -1,7 +1,48 @@
 require './src/character'
 
 RSpec.describe Character do
+  subject { Character.new }
+
   it 'should work' do
     expect(true).to eq(true)
+  end
+
+  it 'should start with 1000 health' do
+    expect(subject.health).to eq(1000)
+  end
+
+  it 'should start at level 1' do
+    expect(subject.level).to eq(1)
+  end
+
+  it 'should start alive' do
+    expect(subject.alive?).to eq(true)
+  end
+
+  it 'should receive damage' do
+    subject.damage(100)
+    expect(subject.health).to eq(900)
+  end
+
+  it 'should die when health is at or below zero' do
+    subject.damage(1000)
+    expect(subject.alive?).to eq(false)
+  end
+
+  it 'can be healed after being damaged' do
+    subject.damage(200)
+    subject.heal(100)
+    expect(subject.health).to eq(900)
+  end
+
+  it 'cannot be healed above current max health' do
+    subject.heal(100)
+    expect(subject.health).to eq(1000)
+  end
+
+  it 'cannot be healed back to life once dead' do
+    subject.damage(1000)
+    subject.heal(200)
+    expect(subject.alive?).to eq(false)
   end
 end
--- src/character.rb
+++ src/character.rb
@@ -1,3 +1,29 @@
 class Character
 
+  def initialize
+    @health = 1000
+  end
+
+  def health
+    @health
+  end
+
+  def level
+    1
+  end
+
+  def alive?
+    @health > 0
+  end
+
+  def damage(amount)
+    @health -= amount
+  end
+
+  def heal(amount)
+    return if @health <= 0
+
+    @health += amount
+    @health = 1000 if @health > 1000
+  end
 end
