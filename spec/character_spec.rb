1f63b6c1d2b229d93c3da168de2977a51b4f5d5e
Chris Johnston <cjohnston@connected.io>
2022-12-08 10:44:45 -0500

    iteration 2

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -1,6 +1,7 @@
 require './src/character'
 
 RSpec.describe Character do
+  let(:player_2) { Character.new }
   subject { Character.new }
 
   it 'should work' do
@@ -20,17 +21,17 @@ RSpec.describe Character do
   end
 
   it 'should receive damage' do
-    subject.damage(100)
+    subject.damage(100, player_2)
     expect(subject.health).to eq(900)
   end
 
   it 'should die when health is at or below zero' do
-    subject.damage(1000)
+    subject.damage(1000, player_2)
     expect(subject.alive?).to eq(false)
   end
 
   it 'can be healed after being damaged' do
-    subject.damage(200)
+    subject.damage(200, player_2)
     subject.heal(100)
     expect(subject.health).to eq(900)
   end
@@ -41,8 +42,25 @@ RSpec.describe Character do
   end
 
   it 'cannot be healed back to life once dead' do
-    subject.damage(1000)
+    subject.damage(1000, player_2)
     subject.heal(200)
     expect(subject.alive?).to eq(false)
+  end
+
+  it 'cannot deal damage to itself' do
+    subject.damage(200, subject)
+    expect(subject.health).to eq(1000)
+  end
+
+  it 'should do 50% more damage when attacker is 5 levels above target' do
+    player_2.level = 6
+    subject.damage(200, player_2)
+    expect(subject.health).to eq(700)
+  end
+
+  it 'should do 50% less damage when attacker is 5 levels below target' do
+    subject.level = 6
+    subject.damage(200, player_2)
+    expect(subject.health).to eq(900)
   end
 end
--- src/character.rb
+++ src/character.rb
@@ -2,6 +2,7 @@ class Character
 
   def initialize
     @health = 1000
+    @level = 1
   end
 
   def health
@@ -9,19 +10,30 @@ class Character
   end
 
   def level
-    1
+    @level
+  end
+
+  def level=(lvl)
+    @level = lvl
   end
 
   def alive?
     @health > 0
   end
 
-  def damage(amount)
-    @health -= amount
+  def damage(amount, attacker)
+    return if attacker == self
+
+    if self.level >= attacker.level + 5
+      @health -= amount / 2
+    else
+      @health -= amount
+      @health -= amount / 2 if attacker.level >= self.level + 5
+    end
   end
 
   def heal(amount)
-    return if @health <= 0
+    return if !alive?
 
     @health += amount
     @health = 1000 if @health > 1000
